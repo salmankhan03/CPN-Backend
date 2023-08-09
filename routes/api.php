8c40705cd355b4383d6776783d75c6f071ac875f
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2023-08-09 21:10:34 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 //     // ...
 // })->middleware(Authenticate::class);
 Route::get('/', [UserController::class, 'index']);
-Route::get('/index2', [UserController::class, 'index']);
+Route::get('/index2', [UserController::class, 'index2']);
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/signup', [UserController::class, 'signUp']);
