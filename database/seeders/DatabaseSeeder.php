6c34e2ab5cad6d3f900c80c447bc1837b1f0da2d
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2023-12-26 14:37:03 +0530

    added seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserRoleSeeder::class
+            UserRoleSeeder::class,
+            ProductBrandsSeeder::class
         ]);
     }
 }
