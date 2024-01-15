31e02b7245d681ebc792baa3e1dd6f534a9e38f6
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-01-15 09:19:00 +0530

    migration update

--- database/migrations/2024_01_12_105027_add_istaxapply_field_to_product_table.php
+++ database/migrations/2024_01_12_105027_add_istaxapply_field_to_product_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->boolean('is_tax_apply')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('is_tax_apply');
         });
     }
