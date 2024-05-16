e374763b1b300b4d0e9b48afa92ec6ebd3e9e42b
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-05-16 15:58:47 +0530

    api update

--- app/Api/Controllers/SliderImageController.php
+++ app/Api/Controllers/SliderImageController.php
@@ -15,28 +15,44 @@ use Illuminate\Support\Facades\Auth;
 class SliderImageController extends Controller
 {
     public function upload(Request $request){
+        try {
+            $data = $request->only([
+                'side'
+            ]);
+    
+            $user = \Auth::user();
+    
+    
+            $files = $_FILES;
+    
+            foreach ($files as  $fileName => $file) {
+    
+                $imageData = [];
+    
+                $image = $request->file($fileName);
+    
+                $imageData['image'] = $image;
+                $imageData['original_name'] = $image->getClientOriginalName();
+                $imageData['created_by'] = $user->id;
+                $imageData['side'] = $data['side'];
+    
+                SliderImages::create($imageData);
+            }
 
-        $data = $request->only([
-            'side'
-        ]);
-
-        $user = \Auth::user();
-
-
-        $files = $_FILES;
-
-        foreach ($files as  $fileName => $file) {
-
-            $imageData = [];
-
-            $image = $request->file($fileName);
-
-            $imageData['image'] = $image;
-            $imageData['original_name'] = $image->getClientOriginalName();
-            $imageData['created_by'] = $user->id;
-            $imageData['side'] = $data['side'];
-
-            SliderImages::create($imageData);
+            $this->response()->json([
+                'status_code' => 200,
+                'message' => 'Slider Images Uploaded Successfully'
+            ]);
         }
+
+        catch (\Exception $e){
+            $this->response()->json([
+                'status_code' => 500,
+                'message' => $e->getMessage()
+            ]);
+        }
+       
+
+
     }
 }
