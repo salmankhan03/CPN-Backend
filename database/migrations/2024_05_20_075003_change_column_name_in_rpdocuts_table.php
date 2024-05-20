4a9f75985d3dc8daa115671e4a894133d22ebb26
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-05-20 13:22:39 +0530

    api update

--- database/migrations/2024_05_20_075003_change_column_name_in_rpdocuts_table.php
+++ database/migrations/2024_05_20_075003_change_column_name_in_rpdocuts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('prdocuts', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->renameColumn('is_feaured', 'is_featured');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('prdocuts', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->renameColumn('is_featured', 'is_feaured');
 
         });
