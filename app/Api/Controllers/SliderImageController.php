a9ebda2dbc368a80e5ca0b0de1d6644b51802389
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-05-27 13:39:06 +0530

    code optimization

--- app/Api/Controllers/SliderImageController.php
+++ app/Api/Controllers/SliderImageController.php
@@ -4,11 +4,7 @@ namespace App\Api\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Role;
-use App\Models\Permission;
-use App\Models\RoleHasPermissions;
 use App\Models\SliderImages;
-use Illuminate\Support\Facades\Auth;
 
 
 
