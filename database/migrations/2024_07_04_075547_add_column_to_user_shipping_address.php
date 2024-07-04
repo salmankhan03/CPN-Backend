691cdb193f9a4fb7ecafb911f4b6f222c5bef5e8
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-07-04 13:29:04 +0530

    adding migrations for table structure change

---
+++ database/migrations/2024_07_04_075547_add_column_to_user_shipping_address.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_shipping_addresses', function (Blueprint $table) {
+            
+            $table->boolean('is_default')->default(0);
+            $table->boolean('is_added_by_user')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_shipping_addresses', function (Blueprint $table) {
+            $table->dropColumn('is_default');
+            $table->dropColumn('is_added_by_user');
+        });
+    }
+};
